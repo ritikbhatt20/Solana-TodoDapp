18dfe6e085b7c1a14e156530e5c7d25f90285518
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-04-29 20:32:47 +0530

    🚀Added walllet button

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,13 +1,23 @@
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
-import { useTodo } from '../hooks/todo'
-import Loading from '../components/Loading'
-import TodoSection from '../components/todo/TodoSection'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic"; // Import dynamic from next/dynamic
+import { useTodo } from '../hooks/todo';
+import Loading from '../components/Loading';
+import TodoSection from '../components/todo/TodoSection';
+import styles from '../styles/Home.module.css';
 
+const DynamicWalletMultiButton = dynamic(
+  () => import("@solana/wallet-adapter-react-ui").then((mod) => mod.WalletMultiButton),
+  { ssr: false } // Disable SSR for WalletMultiButton
+);
 
 const Home = () => {
-    const { initialized, initializeStaticUser, loading, transactionPending, completedTodos, incompleteTodos, addTodo, markTodo, removeTodo, markStaticTodo,removeStaticTodo, addStaticTodo, input,  handleChange } = useTodo()
+    const { initialized, initializeStaticUser, loading, transactionPending, completedTodos, incompleteTodos, addTodo, markTodo, removeTodo, markStaticTodo, removeStaticTodo, addStaticTodo, input, handleChange } = useTodo()
 
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true); // Set isClient to true when component mounts on the client-side
+    }, []);
 
     return (
         <div className={styles.container}>
@@ -17,30 +27,28 @@ const Home = () => {
                         <div className={`${styles.todoCheckbox} ${styles.checked}`} />
                         <div className={styles.inputContainer}>
                             <form onSubmit={addStaticTodo}>
-                                <input value = {input} onChange={handleChange} id={styles.inputField} type="text" placeholder='Create a new todo...' />
+                                <input value={input} onChange={handleChange} id={styles.inputField} type="text" placeholder='Create a new todo...' />
                             </form>
                         </div>
-                        <div className={styles.iconContainer}>
-       
-                        </div>
+                        <div className={styles.iconContainer}></div>
                     </div>
                 ) : (
                     <button type="button" className={styles.button} onClick={() => initializeStaticUser()} disabled={transactionPending}>
                         Initialize
                     </button>
                 )}
-                {/* <WalletMultiButton /> */}
+                {isClient && <DynamicWalletMultiButton />} {/* Render WalletMultiButton only on the client-side */}
             </div>
 
             <div className={styles.mainContainer}>
                 <Loading loading={loading}>
                     <TodoSection title="Tasks" todos={incompleteTodos} action={markStaticTodo} />
-
                     <TodoSection title="Completed" todos={completedTodos} action={removeStaticTodo} />
                 </Loading>
             </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
+
